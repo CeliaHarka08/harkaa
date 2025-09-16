fde221009cdef27b60cd53124a70a4773bbf9b08
CeliaHarka08 <144943163+CeliaHarka08@users.noreply.github.com>
2025-09-16 11:22:24 -0300

    a13

--- script.js
+++ script.js
@@ -19,19 +19,19 @@ const questions = [
         question: "Qual é o maior oceano do planeta?",
         options: ["Oceano Atlântico", "Oceano Índico", "Oceano Ártico", "Oceano Pacífico"],
         answer: "Oceano Pacífico"
-    }
+    },
     {
         question: "Qual é o símbolo químico do ouro?",
         options: ["Ag", "Au", "Fe", "Hh"],
         answer: "Au"
-    }
+    },
     {
         question: "Quem foi o primeiro presidente do Brasil?",
         options: ["Getúlio Vargas", "Juscelino Kubitschek", "Marechal Deodoro da Fonseca", "Dom Pedro II"],
         answer: "Marechal Deodoro da Fonseca"
     },
     {
-        question: "O que significa a sigla "ONU"?",
+        question: "O que significa a sigla ONU?",
         options: ["Organização Nacional das Nações Unidas", "Organização das Nações Unidas", "Organização de Navegação Universal", "Observatório Nacional de Unidades"],
         answer: "Organização das Nações Unidas"
     },
@@ -44,7 +44,7 @@ const questions = [
         question: "Em que continente está localizado o deserto do Saara?",
         options: ["Ásia", "África", "América do Norte", "América do Norte"],
         answer: "África"
-    }
+    },
     {
         question: "Quem escreveu a obra Dom Casmurro?",
         options: ["Machado de Assis", "José de Alencar", "Graciliano Ramos", "Monteiro Lobato"],
